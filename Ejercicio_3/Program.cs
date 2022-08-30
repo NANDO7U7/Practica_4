9be31ee72e60780b0c93e73f73d5abcb50ef3325
FERNANDO JOSE CRUZ CHEVEZ <SMSS141122@ugb.edu.sv>
2022-08-29 21:17:49 -0600

    Agregar archivos de proyecto.

---
+++ Ejercicio_3/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace App3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> lista = new List<string>();
+            Console.WriteLine("Cantidad: {0}", lista.Count);
+            Console.WriteLine("Capacidad: {0}", lista.Capacity);
+            Console.WriteLine();
+
+            for (int i = 0; i < 10; i++)
+            {
+                lista.Add("Dato " + i);
+            }
+            Console.WriteLine("Cantidad: {0}", lista.Count);
+            Console.WriteLine("Capacidad: {0}", lista.Capacity);
+            Console.WriteLine();
+
+            for (int i = 10; i < 33; i++)
+            {
+                lista.Add("Dato " + i);
+            }
+            Console.WriteLine("Cantidad: {0}", lista.Count);
+            Console.WriteLine("Capacidad: {0}", lista.Capacity);
+            Console.WriteLine();
+
+            foreach (string row in lista)
+            {
+                Console.WriteLine(row);
+            }
+            Console.WriteLine("=======Eliminar rango=======");
+            lista.RemoveRange(5, 4);
+
+            foreach (string row in lista)
+            {
+                Console.WriteLine(row);
+            }
+        }
+    }
+}
---
+++ Practica_4/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Practica_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array1 = new int[5];
+            array1[2] = 8;
+            array1[0] = 15;
+
+            for (int i = 0; i < array1.Length; i++)
+            {
+                Console.WriteLine(i + "  " + array1[1]);
+            }
+            for (int i = 0; i < array1.Length; i++)
+            {
+                array1[i] = i * 4;
+            }
+
+            foreach (int row in array1)
+            {
+                Console.WriteLine(row);
+            }
+            string[] paises = new string[5] { "El salvador", "Mexico", "Chile", "Colombia", "Venezuela" };
+            foreach (string pais in paises)
+            {
+                Console.WriteLine(paises);
+            }
+        }
+    }
+}
